b4c2f1587194bc5b1c36fd9b587fbba0a772edc7
rayhankinan <rayhankinan@gmail.com>
2024-02-01 23:13:06 +0700

    Minor changes again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <Layout title="Automated A/B Testing">
-      <div className="flex min-h-screen flex-col items-center justify-center gap-8 py-2">
+      <main className="flex min-h-screen flex-col items-center justify-center gap-8 py-2">
         {/* Display Version ID */}
         <Card>
           <CardHeader>
@@ -81,7 +81,7 @@ export default function Home() {
             )}
           />
         </DisplayWrapper>
-      </div>
+      </main>
     </Layout>
   );
 }
